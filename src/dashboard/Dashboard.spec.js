ba6636d4b843bd6d7677699a1a0deed7ca4fa62e
G H <gh@Gs-MacBook-Pro.local>
2019-10-14 13:59:56 -0700

    test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,22 @@
 // Test away
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+
+test('Dashboard renders correctly', () => {
+    // snapshot test
+    render(< Dashboard />);
+  });
+  
+  test('it render Success! upon success', async () => {
+    
+    const { queryByText, findByText, getByText } = render(< Dashboard />);
+  
+    const successDashboard = queryByText(/dash/i);
+    expect(successDashboard).toBeNull();
+  
+    const button = getByText(/lock gate/i);
+    fireEvent.click(button);
+  });
